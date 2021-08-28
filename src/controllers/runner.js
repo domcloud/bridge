aff8dd411c182743243e5ad65f96a447d408b6c1
Wildan M <willnode@wellosoft.net>
2021-08-28 18:56:42 +0700

    x-callback

--- src/controllers/runner.js
+++ src/controllers/runner.js
@@ -4,11 +4,22 @@ import {
 } from '../util.js';
 import express from 'express';
 import runConfig from '../executor/runner.js';
+import axios from 'axios';
 export default function () {
     var router = express.Router();
     router.post('/', checkAuth, checkGet(['domain']), async function (req, res, next) {
         try {
-            await runConfig(req.body || {}, req.query.domain + "", (s) => res.write(s), false);
+            let stream = '';
+            await runConfig(req.body || {}, req.query.domain + "", (s) => {
+                res.write(s)
+                stream += s;
+            }, false);
+            if (req.header('x-callback'))
+            axios.post(req.header('x-callback'), stream, {
+                headers: {
+                    'content-type': 'text/plain'
+                }
+            });
             res.end();
         } catch (error) {
             res.write(error.message);
