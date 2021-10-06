e7d055a7307075bc6139838f25bb99cb08c7065c
Wildan M <willnode@wellosoft.net>
2021-10-06 16:18:43 +0700

    Init in runner

--- runner.js
+++ runner.js
@@ -1,8 +1,21 @@
-import { runConfigInBackground } from "./src/controllers/runner.js";
+import dotenv from 'dotenv';
+import {
+    runConfigInBackground
+} from "./src/controllers/runner.js";
+import { initUtils } from './src/util.js';
+
+dotenv.config();
+initUtils();
 
 process.on('message', (msg) => {
+    console.log(`Executing`, msg);
     // @ts-ignore
-    const { body, domain, sandbox, callback } = msg;
+    const {
+        body,
+        domain,
+        sandbox,
+        callback
+    } = msg;
     runConfigInBackground(body, domain, sandbox, callback).catch(err => {
         console.error(err);
     });
