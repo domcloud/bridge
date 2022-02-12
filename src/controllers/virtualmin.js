13b9d4b5c99d5937ccfc0d262caaf0f70aef4836
Wildan M <willnode@wellosoft.net>
2022-02-13 06:12:45 +0700

    Typo

--- src/controllers/virtualmin.js
+++ src/controllers/virtualmin.js
@@ -5,7 +5,7 @@ import { checkAuth, checkGet } from '../util.js';
 export default function () {
     var router = express.Router();
     router.get('/create-link', checkAuth, checkGet(['user']), async function (req, res, next) {
-        res.send((await virtualminExec.execFormatted('create-link-login', {
+        res.send((await virtualminExec.execFormatted('create-login-link', {
             user: req.query.user,
         })));
     });
--- tools-init.sh
+++ tools-init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 if [ ! -d "./phpmyadmin" ]; then
     git clone https://github.com/phpmyadmin/phpmyadmin.git phpmyadmin -b STABLE --depth 1
