34fcecf753df1403d7814de5f9e2a3f5e4aa0b49
Wildan M <willnode@wellosoft.net>
2023-03-24 09:30:03 +0700

    Fix catch error on get domain details

--- src/controllers/virtualmin.js
+++ src/controllers/virtualmin.js
@@ -5,15 +5,27 @@ import { checkAuth, checkGet } from '../util.js';
 export default function () {
     var router = express.Router();
     router.get('/create-link', checkAuth, checkGet(['user']), async function (req, res, next) {
-        res.send((await virtualminExec.execFormatted('create-login-link', {
-            user: req.query.user.toString(),
-        })));
+        try {
+            res.send((await virtualminExec.execFormatted('create-login-link', {
+                user: req.query.user.toString(),
+            })));
+        } catch (error) {
+            next(error);
+        }
     });
     router.get('/list-domains', checkAuth, checkGet(['domain']), async function (req, res, next) {
-        res.send(await virtualminExec.getDomainInfo(req.query.domain.toString().split(','), false));
+        try {
+            res.send(await virtualminExec.getDomainInfo(req.query.domain.toString().split(','), false));
+        } catch (error) {
+            next(error);
+        }
     });
     router.get('/list-bandwidth', checkAuth, checkGet(['domain']), async function (req, res, next) {
-        res.send(await virtualminExec.getBandwidthInfo(req.query.domain.toString().split(',')));
+        try {
+            res.send(await virtualminExec.getBandwidthInfo(req.query.domain.toString().split(',')));
+        } catch (error) {
+            next(error);
+        }
     });
     return router;
 }
