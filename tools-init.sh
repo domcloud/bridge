6d809f9ff7fa9df3ecec659ff723ee7b269ab607
Wildan M <willnode@wellosoft.net>
2023-06-30 15:22:05 +0700

    Fix webhook and update

--- tools-init.sh
+++ tools-init.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
 
 if [ ! -d "./phpmyadmin" ]; then
-    git clone https://github.com/phpmyadmin/phpmyadmin.git phpmyadmin -b STABLE --depth 1
+    git clone https://github.com/phpmyadmin/phpmyadmin.git phpmyadmin --filter=tree:0 -b STABLE
     cd ./phpmyadmin
-    composer install --no-dev
-    yarn install --production
+    composer install -o
+    yarn install
     cp config.sample.inc.php config.inc.php
     hash=`node -e "console.log(require('crypto').randomBytes(16).toString('hex'))"`
     sed -ri "s/\['blowfish_secret'\] = '';/['blowfish_secret'] = '${hash}';/g" config.inc.php
     cd ..
+else
+    cd ./phpmyadmin
+    git pull
+    composer install -o
+    yarn install
+    cd ..
 fi
 if [ ! -d "./phppgadmin" ]; then
-    git clone https://github.com/phpPgAdmin2/phpPgAdmin.git phppgadmin --depth 1
+    git clone https://github.com/phpPgAdmin2/phpPgAdmin.git phppgadmin --filter=tree:0
     cd ./phppgadmin
     cp conf/config.inc.php-dist conf/config.inc.php
     sed -i "s/['host'] = ''/['host'] = 'localhost'/" conf/config.inc.php
     sed -i "s/['owned_only'] = false/['owned_only'] = true/g" conf/config.inc.php
     cd ..
+else
+    cd ./phppgadmin
+    git pull
+    cd ..
 fi
 if [ ! -d "./webssh" ]; then
-    git clone https://github.com/huashengdun/webssh.git webssh --depth 1
+    git clone https://github.com/huashengdun/webssh.git webssh --filter=tree:0
     cd ./webssh
+    pip install --user -r requirements.txt
+    cd ..
+else
+    cd ./webssh
+    git pull
     pip install --user -r requirements.txt
     cd ..
 fi
 
 : <<'DISABLED'
 if [ ! -d "./webssh2" ]; then
-    git clone https://github.com/billchurch/webssh2.git webssh2 --depth 1
+    git clone https://github.com/billchurch/webssh2.git webssh2 --filter=tree:0
     cd ./webssh2/app
     npm install --production
     cp config.json.sample config.json
