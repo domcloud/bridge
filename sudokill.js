b6e751e4c4436c2283b9c5e82cc692e6528ebc72
Wildan M <willnode@wellosoft.net>
2023-08-13 19:54:35 +0700

    Update sudokill.js

--- sudokill.js
+++ sudokill.js
@@ -11,14 +11,12 @@ const opts = cli.parse({
     ignore: ['i', 'Ignore user list', 'string', ''],
 });
 
-const output = exec('ps -eo user:20,pid,command --forest --no-headers', {
+const output = exec('ps -eo user:20,pid,etimes,command --forest --no-headers', {
     silent: true,
     fatal: true,
 }).stdout.trim().split('\n');
 
 const ignoreUsers = opts.ignore.split(',')
-    .map(x => x.trim())
-    .filter(x => x.length > 0)
     .reduce((acc, cur) => {
         acc[cur] = true;
         return acc;
@@ -26,27 +24,29 @@ const ignoreUsers = opts.ignore.split(',')
 
 ignoreUsers.root = true;
 
-// scan for any user that is in sd-pam sessions
+// process and filter output
+const splitTest = /^([\w.-]+\+?) +(\d+) +(\d+) (.+)$/;
+const lists = output
+    .map(x => splitTest.exec(x))
+    .filter(x => x !== null && !ignoreUsers[x[1]]).map(match => ({
+        raw: match[0],
+        user: match[1],
+        pid: match[2],
+        etimes: match[3],
+        command: match[4],
+    }));
 
-const sdPamTest = /^([\w.-]+\+?) +\d+  \\_ \(sd-pam\)$/;
-const sdPamUsers = output
-    .map(x => sdPamTest.exec(x))
-    .filter(x => x !== null)
-    .map(x => x[1]);
-
-for (let user of sdPamUsers) {
-    ignoreUsers[user] = true;
+for (const item of lists) {
+    if (item.command === ' \\_ (sd-pam)') {
+        ignoreUsers[item.user] = true;
+    }
 }
 
-const rootPsTest = /^([\w.-]+\+?) +(\d+) \S.+/;
-let candidates = output
-    .map(x => rootPsTest.exec(x))
-    .filter(x => x !== null)
-    .map(x => ({ user: x[1], pid: x[2], proc: x[0] }))
-    .filter(x => !ignoreUsers[x.user]);
+// scan for any processes not in ssh sessions or longer than 3 hours
+let candidates = lists.filter(x => parseInt(x.etimes) > 10800 || (x.command[0] != ' ' && !ignoreUsers[x.user]));
 
 if (opts.test) {
-    console.log(candidates.map(x => x.proc).join('\n'));
+    console.log(candidates.map(x => x.raw).join('\n'));
 } else {
     for (let x of candidates) {
         exec(`kill -9 ${x.pid}`);
