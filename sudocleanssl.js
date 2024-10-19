ec75780d761b4a35a420d8823012e4ec57f31e23
Wildan M <willnode@wellosoft.net>
2024-10-19 13:04:08 +0700

    Fix ssl again

--- sudocleanssl.js
+++ sudocleanssl.js
@@ -5,9 +5,6 @@
 import shelljs from 'shelljs';
 const { exec, ShellString, cat } = shelljs;
 
-// | DOMAIN NAME | PATH TO CERTIFICATE FILE | VALID UNTIL | EXPIRES IN | STATUS |
-const certsExpiryRegexp = /^\| (\S+)\s+\| (\/.+?)\s+\| (.+?)\s+\| (.*?)\s+\| (\S+)\s+\|$/m;
-const cmdListCertsExpiry = 'virtualmin list-certs-expiry --all-domains';
 const cmdListCertsRenewals = 'virtualmin list-domains --name-only --with-feature letsencrypt_renew';
 const askDomainDetailPrefix = 'virtualmin list-domains --simple-multiline --domain ';
 const test = process.env.NODE_ENV == "test";
@@ -26,42 +23,38 @@ function cmd(str) {
     }).stdout.trim();
 }
 
-const listCertsExpiry = cmd(cmdListCertsExpiry).split('\n')
-    .slice(5).map(x => certsExpiryRegexp.exec(x)).filter(x => x);
-const listCertsRenewals = cmd(cmdListCertsRenewals).split('\n');
-
-console.log(`Certs currently active: ${listCertsExpiry.length}, domains in active renewal: ${listCertsRenewals.length}`);
+const listCertsRenewals = cmd(cmdListCertsRenewals).trim().split('\n');
 
 let count = 0;
 
 for (const domain of listCertsRenewals) {
-    const expData = listCertsExpiry.find(x => x[1] == domain);
-    if (!expData) {
-        console.error(`Cert info not found for ${domain}`)
-        continue;
-    }
-    if (expData[5] == 'EXPIRED' || (expData[4].includes(' day') && parseInt(expData[4]) < 30)) {
-        const domainDetail = cmd(askDomainDetailPrefix + domain);
-        const lastIssuedDateExp = domainDetail.match(/Lets Encrypt cert issued: (.+)/);
-        const domainFileExp = domainDetail.match(/File: (.+)/);
-        if (lastIssuedDateExp && domainFileExp) {
-            const lastIssuedDate = Date.parse(lastIssuedDateExp[1]);
-            const domainFile = domainFileExp[1];
-            const deltaHour = Math.trunc(Date.now() - lastIssuedDate / (3600000));
-            if (deltaHour > 24) {
-                if (test) {
-                    console.log(`TEST: Will check ${domain} due to last issue ${deltaHour} hours`);
-                    continue;
-                }
-                console.log(`Disabling renewal for ${domain}`);
-                var c = cat(domainFile).replace(/\nletsencrypt_renew=1/, '');
-                new ShellString(c).to(domainFile);
-                count++;
-            } else {
-                if (test) {
-                    console.log(`TEST: Skipping ${domain} due to last issue ${deltaHour} hours`);
-                }
+    const domainDetail = cmd(askDomainDetailPrefix + domain);
+    const lastIssuedDateStr = domainDetail.match(/Lets Encrypt cert issued: (.+)/);
+    const expiryDateStr = domainDetail.match(/SSL cert expiry: (.+)/);
+    const domainFileStr = domainDetail.match(/File: (.+)/);
+    if (lastIssuedDateStr && expiryDateStr && domainFileStr) {
+        const lastIssuedDate = Date.parse(lastIssuedDateStr[1]);
+        const expiryDate = Date.parse(expiryDateStr[1]);
+        const domainFile = domainFileStr[1];
+        const deltaExp = Math.trunc((expiryDate - Date.now()) / (3600000 * 24));
+        const deltaHour = Math.trunc((Date.now() - lastIssuedDate) / (3600000));
+        if (deltaExp > 30) {
+            if (test) {
+                console.log(`TEST: Skipping ${domain} due to expiry ${deltaHour} days`);
             }
+        } else if (deltaHour < 24) {
+            if (test) {
+                console.log(`TEST: Skipping ${domain} due to last issue ${deltaHour} hours`);
+            }
+        } else {
+            if (test) {
+                console.log(`TEST: Will check ${domain} due to last issue ${deltaHour} hours`);
+                continue;
+            }
+            console.log(`Disabling renewal for ${domain}`);
+            var c = cat(domainFile).replace(/\nletsencrypt_renew=1/, '');
+            new ShellString(c).to(domainFile);
+            count++;
         }
     }
 }
