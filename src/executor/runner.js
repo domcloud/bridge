0b95426ac71d2c56aeca6a8c17c09d0809247c9e
Wildan M <willnode@wellosoft.net>
2024-11-23 09:02:10 +0700

    Use new versioning, fix hashed pw

--- src/executor/runner.js
+++ src/executor/runner.js
@@ -341,10 +341,12 @@ export default async function runConfig(config, domain, writer, sandbox = false)
             if (cb) cb('', 124);
         }, maxExecutionTime).unref();
 
+        const pw = domaindata['Password for mysql'] || domaindata['Password or postgres'] || domaindata['Password'];
+
         await sshExec('unset HISTFILE TERM', false); // https://stackoverflow.com/a/9039154/3908409
         await sshExec(`export CI=true CONTINUOUS_INTEGRATION=true LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 `, false);
         await sshExec(`export PIP_PROGRESS_BAR=off BUILDKIT_PROGRESS=plain`, false);
-        await sshExec(`USERNAME='${domaindata['Username']}' PASSWORD='${domaindata['Password']}'`, false);
+        await sshExec(`USERNAME='${domaindata['Username']}' PASSWORD='${pw}'`, false);
         const firewallOn = await firewallStatus();
         if (config.subdomain) {
             await runConfigSubdomain(config, domaindata, [config.subdomain, domain].join('.'), sshExec, writeLog, virtExec, firewallOn);
