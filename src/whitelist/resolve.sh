d30d72073d89ef596a4d951df4e22050833bb7f7
Wildan M <willnode@wellosoft.net>
2025-03-29 21:57:10 +0700

    Dig from multiple dns

--- src/whitelist/resolve.sh
+++ src/whitelist/resolve.sh
@@ -3,12 +3,19 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 NFIP_ADDRESSES=""
 HOST_ADDRESSES=""
+DNS_SERVERS=("127.0.0.1" "1.1.1.1" "8.8.8.8")
 
 for RECORD_TYPE in A AAAA; do
   while read -r p; do
     if [[ $p != "#"* ]]; then
       printf '\r%s Fetching NS %s of %s' "$(tput el 2> /dev/null)" $RECORD_TYPE $p
-      FFI=$(dig +short $RECORD_TYPE $p @1.1.1.1 | grep -v '\.$')
+      FFI=""
+      for dns in "${DNS_SERVERS[@]}"; do
+        TEMP_RESULT=$(dig +short "$RECORD_TYPE" "$p" @"$dns" | grep -v '\.$')
+        if [[ -n "$TEMP_RESULT" ]]; then
+          FFI+=$'\n'"$TEMP_RESULT"
+        fi
+      done
       if [ -z "$FFI" ] && [ "$RECORD_TYPE" = "A" ]; then
         echo "No records found for $p, exiting."
         exit 1
