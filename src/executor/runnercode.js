6c4a488bc59d4d7480dd5bc11c453940a0c2f814
Wildan M <willnode@wellosoft.net>
2025-04-09 03:47:30 +0700

    Configure host docker

--- src/executor/runnercode.js
+++ src/executor/runnercode.js
@@ -39,8 +39,8 @@ export async function runConfigCodeFeatures(key, value, writeLog, domaindata, ss
                 await sshExec(`sed -i '/DOCKER_HOST=/d' ~/.bashrc`, false);
                 await sshExec(`echo "export DOCKER_HOST=unix:///run/user/$(id -u)/docker.sock" >>  ~/.bashrc;`);
                 await sshExec(`mkdir -p ~/.config/docker  ~/.config/systemd/user/docker.service.d`, false);
-                await sshExec(`printf '{\\n\\t"exec-opts": ["native.cgroupdriver=cgroupfs"]\\n}\\n' > ~/.config/docker/daemon.json`);
-                await sshExec(`printf '[Service]\\nEnvironment="DOCKERD_ROOTLESS_ROOTLESSKIT_NET=pasta"\\nEnvironment="DOCKERD_ROOTLESS_ROOTLESSKIT_PORT_DRIVER=implicit"\\n' > ~/.config/systemd/user/docker.service.d/override.conf`);
+                await sshExec(`printf '{\\n\\t"exec-opts": ["native.cgroupdriver=cgroupfs"],\\n\\t"": "host-gateway-ip": "10.0.2.2"\\n}\\n' > ~/.config/docker/daemon.json`);
+                await sshExec(`printf '[Service]\\nEnvironment="DOCKERD_ROOTLESS_ROOTLESSKIT_NET=pasta"\\nEnvironment="DOCKERD_ROOTLESS_ROOTLESSKIT_PORT_DRIVER=implicit"\\nEnvironment="DOCKERD_ROOTLESS_ROOTLESSKIT_DISABLE_HOST_LOOPBACK=false"\\n' > ~/.config/systemd/user/docker.service.d/override.conf`);
                 await sshExec(`dockerd-rootless-setuptool.sh install --skip-iptables`);
                 await sshExec(`export DOCKER_HOST=unix:///run/user/$(id -u)/docker.sock`, false);
             } else if (value === 'off') {
