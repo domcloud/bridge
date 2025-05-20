06b95fcd99510c67eed25c7a0afe69db6ddf4560
Wildan M <willnode@wellosoft.net>
2025-05-20 09:56:43 +0700

    Skip gmail smtp v6

--- src/whitelist/resolve.sh
+++ src/whitelist/resolve.sh
@@ -6,33 +6,41 @@ HOST_ADDRESSES=""
 DNS_SERVERS=("127.0.0.1" "1.1.1.1")
 
 for RECORD_TYPE in A AAAA; do
-  while read -r p; do
-    if [[ $p != "#"* ]]; then
-      printf '\r%s Fetching NS %s of %s' "$(tput el 2> /dev/null)" $RECORD_TYPE $p
-      FFI=""
-      for dns in "${DNS_SERVERS[@]}"; do
-        TEMP_RESULT=$(dig +short "$RECORD_TYPE" "$p" @"$dns" | grep -v '\.$')
-        if [[ -n "$TEMP_RESULT" ]]; then
-          FFI+=$'\n'"$TEMP_RESULT"
-        fi
-      done
-      if [ -z "$FFI" ] && [ "$RECORD_TYPE" = "A" ]; then
-        echo "No records found for $p, exiting."
-        exit 1
-      fi
-      while read -r q; do
-        if [[ $q != "" ]]; then
-          HOST_ADDRESSES+="$q $p"$'\n'
-          if [[ $RECORD_TYPE == "A" ]]; then
-            NFIP_ADDRESSES+="add element inet filter whitelist { $q }"$'\n'
-          else
-            NFIP_ADDRESSES+="add element inet filter whitelist-v6 { $q }"$'\n'
-          fi
-        fi
-      done <<< "$FFI"
+  while read -r line; do
+    [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue  # skip comments and empty lines
+
+    # Check for 4: prefix
+    if [[ "$line" == 4:* ]]; then
+      domain="${line#4:}"
+      [[ "$RECORD_TYPE" != "A" ]] && continue  # skip AAAA if 4: prefix
+    else
+      domain="$line"
     fi
-  done <"$SCRIPT_DIR/sites.conf"
-done 
+
+    printf '\r%s Fetching NS %s of %s' "$(tput el 2> /dev/null)" "$RECORD_TYPE" "$domain"
+    FFI=""
+    for dns in "${DNS_SERVERS[@]}"; do
+      TEMP_RESULT=$(dig +short "$RECORD_TYPE" "$domain" @"$dns" | grep -v '\.$')
+      if [[ -n "$TEMP_RESULT" ]]; then
+        FFI+=$'\n'"$TEMP_RESULT"
+      fi
+    done
+    if [ -z "$FFI" ] && [ "$RECORD_TYPE" = "A" ]; then
+      echo "No records found for $domain, exiting."
+      exit 1
+    fi
+    while read -r q; do
+      if [[ -n "$q" ]]; then
+        HOST_ADDRESSES+="$q $domain"$'\n'
+        if [[ $RECORD_TYPE == "A" ]]; then
+          NFIP_ADDRESSES+="add element inet filter whitelist { $q }"$'\n'
+        else
+          NFIP_ADDRESSES+="add element inet filter whitelist-v6 { $q }"$'\n'
+        fi
+      fi
+    done <<< "$FFI"
+  done < "$SCRIPT_DIR/sites.conf"
+done
 
 echo ""
 echo -n "$NFIP_ADDRESSES" > "$SCRIPT_DIR/nfip_addresses.txt"
